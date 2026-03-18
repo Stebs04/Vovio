aa65ad3ee90158259e140b2df40aca6c23467d99
Stefano <stefanobellan94@gmail.com>
2026-03-18 20:54:29 +0100

    feat: realizzazione e implementazione dell'agent per la trascrizione di un video

--- apps/backend/utils/video_processing.py
+++ apps/backend/utils/video_processing.py
@@ -1,11 +1,22 @@
 #Importazione del modulo di editing video per la gestione dei flussi multimediali
 from moviepy import VideoFileClip
 
-def extract_audio(video_path: str, output_audio_path: str):
+from pathlib import Path
+
+#Importazione del modulo di configurazione dei path di processamento
+import config
+
+def extract_audio(video_path: str):
+    #Generazione dello stem
+    stem = Path(video_path).stem
+    #Definizione del percorso di destinazione
+    audio_output = config.TEMP_DIR / f"{stem}.wav"
     #Utilizzo di un Context Manager per garantire il rilascio automatico delle risorse di sistema, indipendentemente dall'esito del processo.
     with VideoFileClip(video_path) as video:
         # Isolamento del flusso audio e persistenza su disco nel formato di output specificato.
-        video.audio.write_audiofile(output_audio_path)
+        video.audio.write_audiofile(str(audio_output))
+    #Restituisco il percorso generato dalla pipeline di trascrizione
+    return audio_output
 
 
 
